09ef2079c902dcb461cecb634800d7fcfce3285a
Remdran <kelsyerg@gmail.com>
2017-09-07 11:00:43 +0100

    Initial Setup

--- FizzBuzz/spec/FizzBuzzSpec.php
+++ FizzBuzz/spec/FizzBuzzSpec.php
@@ -16,4 +16,16 @@ class FizzBuzzSpec extends ObjectBehavior
     function it_translates_1_for_fizzbuzz() {
         $this->execute(1)->shouldReturn(1);
     }
+
+    // function it_translates_3_for_fizzbuzz() {
+    //     $this->execute(1)->shouldReturn(fizz);
+    // }
+
+    // function it_translates_5_for_fizzbuzz() {
+    //     $this->execute(5)->shouldReturn(buzz);
+    // }
+
+    // function it_translates_15_for_fizzbuzz() {
+    //     $this->execute(15)->shouldReturn(fizzbuzz);
+    // }
 }
